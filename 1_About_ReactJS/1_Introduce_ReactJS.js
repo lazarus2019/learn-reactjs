9f10b512025ef836d9f937aea0cf0a2812f38e2a
NTSon <thaisonbk2020@gmail.com>
2022-07-06 18:51:14 +0700

    Upload About ReactJS

---
+++ 1_About_ReactJS/1_Introduce_ReactJS.js
@@ -0,0 +1,13 @@
+/*
+    ReactJS
+    
+    Tại sao nên học React?
+        - Độ hot
+        - Cộng đồng lớn
+        - Được đánh giá là thân thiện với SEO trong những Lib/FW JS (SEO - Search Engine Optimization)
+        - Khả năng mở rộng tốt, tái sử dụng cao
+        - Hiệu xuất cao
+        - Phát triển nhanh chóng
+        - Khả năng tương thích ngược
+        - Tương lai sáng
+*/
---
+++ 1_About_ReactJS/2_SPA_MPA.js
@@ -0,0 +1,57 @@
+/*
+    # SPA - Single-Page Application
+    - ReactJS là 1 trong những thư viên tạo ra SPA
+    - Các "ông lớn" sử dụng SPA: Google, Facebook, Twitter
+    - Các SPA khác: F8, Shoppe, 30shine, chotot, zingmp3,..
+
+    ## Cách triển khai
+    - SPA - Single-Page Application -> CSR - Client Side rendering
+    - MPA - Multi-Page Application -> SSR -> Server side rendering
+
+    ---
+    ## Sự khác biệt
+
+    ### SPA:
+        - Được cho là cách tiếp cận hiện đại hơn
+        - Không yêu cầu tải lại trang trong quá trình sử dụng
+
+    ### MPA:
+        - Là cách tiếp cận cổ điển hơn
+        - Tải lại trang trong quá trình sử dụng (click vào đường link, chuyển trang,...)
+
+    ## So sánh
+
+    ### Tốc độ
+    - SPA nhanh hơn khi sử dụng
+        - Phần lớn tài nguyên được tải trong lần đầu
+        - Trang chỉ tải thêm dữ liệu khi cần
+    - MPA chậm hơn khi sử dụng
+        - Luôn tải lại toàn bộ trang khi truy cập và chuyển hướng
+    
+    ### Bóc tách
+    - SPA có phần Front-end riêng biệt
+    - MPA Front-end & Back-end phụ thuộc nhau nhiều hơn, được đặt trong cùng 1 dự án
+
+    ### SEO
+    - SPA không thân thiện với SEO như MPA
+    - Trải nghiệm trên thiết bị di động tốt hơn
+    
+    ### UX
+    - SPA cho trải nghiệm tốt hơn, nhất là các thao tác chuyển trang
+    - Trải nghiệm trên thiết bị di động tốt hơn
+
+    ### Quá trình phát triển
+    - SPA dễ dàng tái sử dụng code (component)
+    - SPA bóc tách FE & BE
+        - Chia team phát triển song song
+        - Phát triển thêm mobile app dễ dàng
+
+    ### Phụ thuộc Javascript
+    - SPA phụ thuộc hoàn toàn vào Javascript
+    - MPA có thể không cần Javascript
+
+    ---
+
+    ## Chọn SPA hay MPA
+    - Không có thứ gì luôn hoàn hảo trong mọi trường hợp
+*/
