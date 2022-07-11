796a23ca661fe9e594613ac8117c081d68c0bc42
NTSon <thaisonbk2020@gmail.com>
2022-07-11 16:56:22 +0700

    Upload project tiktok init

---
+++ 4_React_project/tiktok/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+// Import components
+import LearnUseState from './components/LearnUseState.js'
+
+const orders = [100, 50, 20]
+function App() {
+  return (
+    <LearnUseState />
+  );
+}
+
+export default App;
